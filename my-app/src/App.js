864570ee4d3f2607f0b88b28eb1947edbebfbbf9
KevinH48264 <1kevin.huang@gmail.com>
2020-12-09 10:20:31 -0500

    removed submodule for my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import Landing from './containers/landing/landing';
+import Register from './containers/register/Register';
+import Login from './containers/login/login';
+import Roster from './containers/roster/Roster';
+import Profile from './containers/profile/profile.jsx';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path = "/">
+            <Landing/>
+          </Route>
+          <Route exact path = "/register">
+            <Register/>
+          </Route>
+          <Route exact path = "/login">
+            <Login/>
+          </Route>
+          <Route exact path = "/roster">
+            <Roster/>
+          </Route>
+          <Route exact path = "/profile/:userid">
+            <Profile/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
