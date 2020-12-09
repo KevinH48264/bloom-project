119060843b1e09d2c3221774fd747fab5830ee8f
KevinH48264 <1kevin.huang@gmail.com>
2020-12-09 11:43:39 -0500

    temp1

---
+++ build/precache-manifest.24a13b86a635e78ec5d3a79e0d19ab07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a40a6046ec79b2c49630f7cc7bdfbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb4c5a86514a6545a52",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4ae42840ff6fddff5a08",
+    "url": "/static/css/main.a856910a.chunk.css"
+  },
+  {
+    "revision": "2cb4c5a86514a6545a52",
+    "url": "/static/js/2.8518f028.chunk.js"
+  },
+  {
+    "revision": "3263eb4fa2a4ed54bb570011085a455d",
+    "url": "/static/js/2.8518f028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae42840ff6fddff5a08",
+    "url": "/static/js/main.585d28f8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "44563394b3805b79bf2b4f0a877c2d78",
+    "url": "/static/media/logo.44563394.png"
+  }
+]);
