dbeffb86c85254b58c8aef12004df1adb006d700
KevinH48264 <1kevin.huang@gmail.com>
2020-12-09 22:21:29 -0500

    test

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb+srv://test:HDAG%21%40%23%24@cluster0.gri4u.mongodb.net/bloom?retryWrites=true&w=majority"
+    url: "mongodb+srv://test:HDAG%21%40%23%24@cluster0.gri4u.mongodb.net/bloom?retryWrites=true&w=majority",
+    REACT_APP_BACKEND_URL: "https://bloom-website.herokuapp.com/"
 };
