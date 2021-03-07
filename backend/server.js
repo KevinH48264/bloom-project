9dd0200d565291de04261e9278f16e6c5658167c
KevinH48264 <1kevin.huang@gmail.com>
2021-03-07 00:12:58 -0500

    revert to refresh not working

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const db = require("./models");
 
 app.use(express.static(path.join(__dirname, '../build')))
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build'), (err) => {
       if (err) {
         res.status(500).send(err)
