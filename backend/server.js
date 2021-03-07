b6ff3497aa816ceeda7291f656d3db012006b14f
KevinH48264 <1kevin.huang@gmail.com>
2021-03-06 23:28:23 -0500

    added /

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./models");
 
 app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build'))
 })
 
