c68fc9bb456a8c0cefd88007e3994fcaf6d19401
KevinH48264 <1kevin.huang@gmail.com>
2021-03-06 23:49:05 -0500

    changed /* in server to build

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // connecting to MongoDB
 const db = require("./models");
 
-// app.use(express.static(path.join(__dirname, '../build')))
-app.use("/bloom-project/", express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '../build')))
+// app.use("/bloom-project/", express.static(path.join(__dirname, '../build')))
 
 // app.get('/*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../build'))
